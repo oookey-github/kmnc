327858c5409c7ccc0803b53affc371aa0b8a43dd
平賀 舞子 <hiraga@oookey.co.jp>
2023-02-09 20:57:30 +0900

    【html】index.html guide.html編集 【css】style.css guide.css restaurant.css編集 【js】main.js編集 【img】画像追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// ページ内リンクスクロール
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
 // ページトップボタン
 const pagetopBtn = document.querySelector('.page-top');
 pagetopBtn.addEventListener('click',  function(){
@@ -15,3 +23,14 @@ pagetopBtn.addEventListener('click',  function(){
 //         pagetopBtn.style.opacity = "0";
 //     }
 // }
+
+// ハンバーガーメニュー
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
