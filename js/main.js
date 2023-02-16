f68cb2a4bd0807f4f0cfe61c7100cdbb6c4b2c7a
平賀 舞子 <hiraga@oookey.co.jp>
2023-02-16 18:17:57 +0900

    【html】index.htm slider_2.htmll編集 【css】top.css編集 【js】main.js編集 slick.js追加

--- js/main.js
+++ js/main.js
@@ -34,3 +34,48 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+
+// 特別プラン
+$(function(){
+    $('.slick').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide:5,
+        autoplaySpeed:1500,
+        arrows:false             //追加（矢印非表示
+        
+    });
+});
+
+// 心揺さぶる体験
+$(function(){
+    $('.slick_exp').slick({
+        autoplay: true,//自動的に動き出すか。初期値はfalse。
+        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+        speed: 1100,//スライドのスピード。初期値は300。
+        autoplaySpeed:1700,
+        slidesToShow: 1,//スライドを画面に3枚見せる
+        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+        prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+        nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+        centerMode: true,//要素を中央ぞろえにする
+        variableWidth: true,//幅の違う画像の高さを揃えて表示
+        dots: true,//下部ドットナビゲーションの表示
+        dotsClass: "slick_exp-dots",
+        asNavFor:".text-slider"
+    });
+});
+$(function(){
+    $('.text-slider').slick({
+        speed: 1100,//スライドのスピード。初期値は300。
+        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+        dots: false,//下部ドットナビゲーションの表示
+        draggable:false,         //追加（ドラッグでのスライド禁止
+        arrows:false             //追加（矢印非表示
+    });
+});
