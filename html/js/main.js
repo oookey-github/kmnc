ec656d5a5e9342b262572c6e42495c1f84c24eba
Oookey Inc.山口咲綾 <yamaguchi@oookey.co.jp>
2023-02-22 14:02:56 +0900

    【js】main.js編集

--- html/js/main.js
+++ html/js/main.js
@@ -109,15 +109,15 @@ new simpleParallax(image, {
 });
 
 $(function () {
-  $(window).scroll(function () {
-    const windowHeight = $(window).height();
-    const scroll = $(window).scrollTop();
+    $(window).scroll(function () {
+        const windowHeight = $(window).height();
+        const scroll = $(window).scrollTop();
 
-    $('.fadein').each(function () {
-      const targetPosition = $(this).offset().top;
-      if (scroll > targetPosition - windowHeight + 200) {
-        $(this).addClass("is-fadein");
-      }
+        $('.fadein').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 200) {
+            $(this).addClass("is-fadein");
+        }
+        });
     });
-  });
 });
