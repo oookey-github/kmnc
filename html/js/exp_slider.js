da917dbb6fc8118dd3a97bd596cf5ed8b1a2fcc4
平賀 舞子 <hiraga@oookey.co.jp>
2023-03-02 19:59:56 +0900

    【html】experience.html news_detail.html stargazing.html編集 【css】guide.css news.css slick.css style_special.css編集 【js】exp_slider.js追加 slick.min.js編集 【img】画像追加

---
+++ html/js/exp_slider.js
@@ -0,0 +1,15 @@
+// 心揺さぶる体験
+$('.fv_slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 100,//次のスライドに切り替わる待ち時間
+    speed:2000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: false,//左右の矢印あり
+    dots: false,//下部ドットナビゲーションの表示
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
