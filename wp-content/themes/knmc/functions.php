8a1474a184e73bbd4f9c35c8d1812ffa3452198e
LO KA WENG <weng@oookey.co.jp>
2023-03-03 12:22:12 +0900

    【WP】functions

--- wp-content/themes/knmc/functions.php
+++ wp-content/themes/knmc/functions.php
@@ -11,15 +11,14 @@ function custom_pagination_html( $template ) {
 add_filter('navigation_markup_template','custom_pagination_html');
 
 // ページ表示件数の設定
-// function custom_posts_per_page($query) {
-//     if(is_admin() || ! $query->is_main_query()){
-//         return;
-//     }
-//     // お知らせ
-//     if($query->is_archive('news')) {
-//         $query->set('posts_per_page', '2');
-//     }
-// }
-// add_action('pre_get_posts', 'custom_posts_per_page');
+function custom_posts_per_page($query) {
+    if ( !is_admin() && $query->is_main_query()) {
+		if ( is_post_type_archive('news') ) {
+			$query->set( 'posts_per_page' , 10 );//表示したい数
+		}
+	}
+	return $query;
+}
+add_action('pre_get_posts', 'custom_posts_per_page');
 
 ?>
