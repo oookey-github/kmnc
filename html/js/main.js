b5d3e2ed34b33a06c7abb47e4ccb444f84367f10
平賀 舞子 <hiraga@oookey.co.jp>
2023-03-03 13:07:19 +0900

    【html】index.html experience.html guide.html news_detail.html news_list.html stargazing.html tourism.html編集 【css】experience.css style_spesial.css top.css tourism.css編集 【js】main.js編集

--- html/js/main.js
+++ html/js/main.js
@@ -10,9 +10,10 @@
 $(function(){
     $('a[href^="#"]').click(function(){
     var href= $(this).attr("href");
+    var speed = 1100;
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, 1500);
+    $("html, body").animate({scrollTop:position}, speed, 'swing');
     return false;
 });
 });
