ad9669329e1aeb96992d26583a6666136e49f540
Oookey Inc.山口咲綾 <yamaguchi@oookey.co.jp>
2023-03-14 12:21:11 +0900

    【js】main.js編集

--- html/js/main.js
+++ html/js/main.js
@@ -214,7 +214,7 @@ $(function(){
     $('.vertical_text p') 
     .css({opacity:0})
     .each(function(i){
-        $(this).delay(500 * i).animate({opacity:1}, 1200);
+        $(this).delay(500 * i).animate({opacity:1}, 1500);
     })
 })
 // // fvフェードイン
